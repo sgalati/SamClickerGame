f3760a427f3937fbd7329af69ca458b20c662113
Samantha Galati 2019 <sgalati9@3012-l29530-s.local>
2019-05-08 12:24:22 -0500

    Initial Commit

---
+++ CLICKERGAME/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CLICKERGAME
+//jjjjjj
+//  Created by Samantha Galati 2019 on 5/8/19.
+//  Copyright © 2019 Samantha Galati 2019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
